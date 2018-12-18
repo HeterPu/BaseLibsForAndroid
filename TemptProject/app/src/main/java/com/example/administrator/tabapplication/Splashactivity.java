ea270b833ef62e182094f61b8cbd8ca81ed42b4d
HeterPu <wycgpeterhu@sina.com>
2018-12-18 21:05:29 +0800

    uptimize push animation

--- TemptProject/app/src/main/java/com/example/administrator/tabapplication/Splashactivity.java
+++ TemptProject/app/src/main/java/com/example/administrator/tabapplication/Splashactivity.java
@@ -27,7 +27,7 @@ public class Splashactivity extends BaseApplicationActivity {
             @Override
             public void run() {
 
-                Intent rr = new Intent(Splashactivity.this,MainActivity.class);
+                Intent rr = new Intent(Splashactivity.this,PermissionTestActivity.class);
                 Splashactivity.this.pushActivity(rr,false);
                 Splashactivity.this.finish();
             }
